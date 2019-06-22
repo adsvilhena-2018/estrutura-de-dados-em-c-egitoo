8b22df4f74bcb237cbd2facab9a02b0b097fae66
egitoo <45129357+egitoo@users.noreply.github.com>
2019-06-22 15:46:04 -0400

    Update minheap.c

--- heap/arranjos/minheap.c
+++ heap/arranjos/minheap.c
@@ -5,13 +5,13 @@ void minHeap(int vetor[], int tamanho, int i) {
     int esq = 2 * i + 1;
     int dir = 2 * i + 2;
 
-    if (esq < tamanho && vetor[esq] > vetor[menor])
+    if(esq < tamanho && vetor[esq] > vetor[menor])
         menor = esq;
 
-    if (dir < tamanho && vetor[dir] > vetor[menor])
+    if(dir < tamanho && vetor[dir] > vetor[menor])
         menor = dir;
 
-    if (menor != i) {
+    if(menor != i) {
         int aux = vetor[i];
         vetor[i] = vetor[menor];
         vetor[menor] = aux;
@@ -22,10 +22,10 @@ void minHeap(int vetor[], int tamanho, int i) {
 
 void ordena(int vetor[], int tamanho) {
 
-    for (int i = tamanho / 2 - 1; i >= 0; i--)
+    for(int i = tamanho / 2 - 1; i >= 0; i--)
         minHeap(vetor, tamanho, i);
 
-    for (int i=tamanho-1; i>=0; i--) {
+    for(int i=tamanho-1; i>=0; i--) {
         int temp = vetor[0];
         vetor[0] = vetor[i];
         vetor[i] = temp;
