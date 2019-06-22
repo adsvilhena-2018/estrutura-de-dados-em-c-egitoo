31c455558629cdc31d5cfc3299c1d245da0e2abd
egitoo <45129357+egitoo@users.noreply.github.com>
2019-06-22 13:54:52 -0400

    Update maxheap.c

---
+++ heap/arranjos/maxheap.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+void minHeap(int vetor[], int tamanho, int i) {
+    int maior = i;
+    int esq = 2 * i + 1;
+    int dir = 2 * i + 2;
+
+    if (esq < tamanho && vetor[esq] > vetor[maior])
+        maior = esq;
+
+    if (dir < tamanho && vetor[dir] > vetor[maior])
+        maior = dir;
+
+    if (maior != i)
+    {
+        int aux = vetor[i];
+        vetor[i] = vetor[maior];
+        vetor[maior] = aux;
+
+        minHeap(vetor, tamanho, maior);
+    }
+}
+
+void ordena(int vetor[], int tamanho) {
+
+    for (int i = tamanho / 2 - 1; i >= 0; i--)
+        minHeap(vetor, tamanho, i);
+
+    for (int i=tamanho-1; i>=0; i--)
+    {
+        int temp = vetor[0];
+        vetor[0] = vetor[i];
+        vetor[i] = temp;
+
+        minHeap(vetor, i, 0);
+    }
+}
+
+void imprime(int vetor[], int tamanho) {
+    for(int i = 0; i < tamanho; i++)
+        printf("%d, ", vetor[i]);
+}
+
+
+void main() {
+    int vetor[] = {12, 11, 13, 5, 6, 99, 4, 556, 125};
+    int tamanho = sizeof(vetor) / sizeof(int);
+
+    ordena(vetor, tamanho);
+    printf("Tamanho do vetor: %d\n\n.:Vetor organizado:.\n", tamanho);
+    imprime(vetor, tamanho);
+}
